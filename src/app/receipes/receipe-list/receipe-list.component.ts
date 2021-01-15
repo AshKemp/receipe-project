ff114d510d0b4294d5e71553500b9eb1bbdc5829
Ashwin Km <kmuraliashwin@gmail.com>
2021-01-15 17:37:14 +0530

    Instantiated Recipes

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Receipe } from '../receipe.model';
 
 @Component({
   selector: 'app-receipe-list',
   templateUrl: './receipe-list.component.html',
-  styleUrls: ['./receipe-list.component.css']
+  styleUrls: ['./receipe-list.component.css'],
 })
 export class ReceipeListComponent implements OnInit {
+  receipes: Receipe[] = [
+    new Receipe(
+      'Test Receipe',
+      'This is a simple test receipe',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Receipe(
+      'Test Receipe2',
+      'This is a simple test receipe',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
