4fbc1ff3bdcca4941943a50a21f649ff6576e1d1
Ashwin Km <kmuraliashwin@gmail.com>
2021-01-15 12:52:39 +0530

    Created Receipes, Receipes Details, Receipes List, Receipes Item and Shopping List, Shopping List Edit Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ReceipesComponent, ReceipeListComponent, ReceipeDetailComponent, ReceipeItemComponent, ShoppingListComponent, ShoppingListEditComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
